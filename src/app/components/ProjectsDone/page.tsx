6e73f9a7dcb3730bfa5f98cf58bcd90ec7fdf155
sylvesterakrong <akrongsylvester@gmail.com>
2023-10-21 17:20:35 +0000

    url/projects update

--- src/app/components/ProjectsDone/page.tsx
+++ src/app/components/ProjectsDone/page.tsx
@@ -8,7 +8,7 @@ const projectsdone = () => {
       <div>
         <h3 className='text-2xl font-medium text-teal-600'>Some Projects I have built</h3>
       </div>
-      <div className='lg:flex gap-10'>
+      <div className='lg:flex gap-10 justify-center items-center'>
         <Link href={'https://elegantbuildersltd.vercel.app/'}>
           <div className='text-center shadow-lg p-10 rounded-xl my-10'>
             <Image src={"/assets/projects/web1.jpg"} width='100' height='100' alt="logo" objectFit='cover' className='mix-blend-multiply items-center justify-center'/>
@@ -23,7 +23,7 @@ const projectsdone = () => {
           </div>
         </Link>
 
-        <Link href={'BlogTree.vercel.app'}>
+        <Link href={'https://blogtree.vercel.app/'}>
           <div className='text-center shadow-lg p-10 rounded-xl my-10'>
             <Image src={"/assets/projects/web2.jpg"} width='100' height='100' alt="logo" objectFit='cover' className='mix-blend-multiply items-center justify-center'/>
             <h3 className='text-lg font-medium pt-8 pb-2'>BlogTREE</h3>
@@ -41,7 +41,7 @@ const projectsdone = () => {
           <Image src={"/assets/projects/web3.jpg"} width='100' height='100' alt="logo" objectFit='cover' className='mix-blend-multiply items-center justify-center'/>
           <h3 className='text-lg font-medium pt-8 pb-2'>Malici-Chrome Extension</h3>
           <p className='py-2'>
-            An extension made to detect malicious URLs easily when typed into the extension
+            An extension made to detect malicious URLs <br></br>easily when typed into the extension
           </p>
           <h4 className='py-4 text-teal-600'>Tools i used</h4>
           <p className='text-gray-800 py-1'>Html</p>
