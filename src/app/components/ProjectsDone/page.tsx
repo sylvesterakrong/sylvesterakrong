0044bd0f06cd010e08a80a020ae170484e33190e
sylvesterakrong <akrongsylvester@gmail.com>
2023-10-21 18:08:37 +0000

    pages update

--- src/app/components/ProjectsDone/page.tsx
+++ src/app/components/ProjectsDone/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const projectsdone = () => {
   return (
-    <section>
+    <section className='pt-20'>
       <div>
         <h3 className='text-2xl font-medium text-teal-600'>Some Projects I have built</h3>
       </div>
@@ -37,17 +37,21 @@ const projectsdone = () => {
           </div>
         </Link>
 
-        <div className='text-center shadow-lg p-10 rounded-xl my-10'>
-          <Image src={"/assets/projects/web3.jpg"} width='100' height='100' alt="logo" objectFit='cover' className='mix-blend-multiply items-center justify-center'/>
-          <h3 className='text-lg font-medium pt-8 pb-2'>Malici-Chrome Extension</h3>
-          <p className='py-2'>
-            An extension made to detect malicious URLs <br></br>easily when typed into the extension
-          </p>
-          <h4 className='py-4 text-teal-600'>Tools i used</h4>
-          <p className='text-gray-800 py-1'>Html</p>
-          <p className='text-gray-800 py-1'>CSS & Javascript</p>
-          <p className='text-gray-800 py-1'>Manifest.Json</p>
-        </div>
+
+        <Link href={'https://github.com/sylvesterakrong/Malici-Chrome-extension'}>
+          <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+            <Image src={"/assets/projects/web3.jpg"} width='100' height='100' alt="logo" objectFit='cover' className='mix-blend-multiply items-center justify-center'/>
+            <h3 className='text-lg font-medium pt-8 pb-2'>Malici-Chrome Extension</h3>
+            <p className='py-2'>
+              An extension made to detect malicious URLs <br></br>easily when typed into the extension
+            </p>
+            <h4 className='py-4 text-teal-600'>Tools i used</h4>
+            <p className='text-gray-800 py-1'>Html</p>
+            <p className='text-gray-800 py-1'>CSS & Javascript</p>
+            <p className='text-gray-800 py-1'>Manifest.Json</p>
+          </div>
+        </Link>
+       
       </div>
     </section>
   )
